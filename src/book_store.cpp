794231d55503b6da7a119a23a70d90c3bccffcbf
Elina <elina.malygina@yandex.ru>
2021-03-21 23:35:08 +0300

    commit

--- src/book_store.cpp
+++ src/book_store.cpp
@@ -8,7 +8,23 @@ ResizeStorageStatus resize_storage(Book *&storage, int size, int new_capacity) {
   // здесь мог бы быть ваш разносторонний и многогранный код ...
   // Tip 1: проведите валидацию аргументов функции
   // Tip 2: не забудьте высвободить ранее выделенную память под хранилище
-  return ResizeStorageStatus::SUCCESS;
+    if (storage == nullptr) {
+        return ResizeStorageStatus::NULL_STORAGE;
+    }
+    if (size < 0) {
+        return ResizeStorageStatus::NEGATIVE_SIZE;
+    }
+    if (new_capacity <= size) {
+        return ResizeStorageStatus::INSUFFICIENT_CAPACITY;
+    }
+
+    Book *new_storage = new Book[new_capacity]{};
+    for (int i = 0; i < size; i++) {
+        new_storage[i] = storage[i];
+    }
+    delete[] storage;
+    storage = new_storage;
+    return ResizeStorageStatus::SUCCESS;
 }
 
 // 2. реализуйте конструктор ...
@@ -17,23 +33,35 @@ BookStore::BookStore(const std::string &name) : name_{name} {
   if (name.empty()) {
     throw std::invalid_argument("BookStore::name must not be empty");
   }
-
   // здесь мог бы быть ваш сотрясающий землю и выделяющий память код ...
+    this -> name_ = name;
+    storage_capacity_ = kInitStorageCapacity;
+    storage_ = new Book[storage_capacity_];
 }
 
 // 3. реализуйте деструктор ...
 BookStore::~BookStore() {
   // здесь мог бы быть ваш высвобождающий разум от негатива код ...
   // Tip 1: я свободен ..., словно память в куче: не забудьте обнулить указатель
+    if (storage_ != nullptr)
+    {
+        delete[] storage_;
+        storage_ = nullptr;
+    }
+    storage_capacity_ = 0;
+    storage_size_ = 0;
 }
 
 // 4. реализуйте метод ...
 void BookStore::AddBook(const Book &book) {
   if (storage_size_ == storage_capacity_) {
     // здесь мог бы быть ваш умопомрачительный код ...
-    // Tip 1: используйте функцию resize_storage_internal, задав новый размер хранилища
-    // Tip 2: не забудьте обработать статус вызова функции
-  }
+        resize_storage_internal(storage_capacity_ + kCapacityCoefficient);
+        // Tip 1: используйте функцию resize_storage_internal, задав новый размер хранилища
+        // Tip 2: не забудьте обработать статус вызова функции
+    }
+    storage_[storage_size_] = book;
+    storage_size_+=1;
   // Tip 3: не забудьте добавить книгу в наше бездонное хранилище ...
 }
 
